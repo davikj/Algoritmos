7b139381a15e56c337a4c42a34286e44d44cba2a
davikj <david.beltrami@mail.udp.cl>
2017-05-23 12:27:15 -0400

    Fibonacci recursivo

---
+++ algoritmo2.py
@@ -0,0 +1,10 @@
+import sys
+sys.setrecursionlimit(1000000000)
+print("Unendlicher Fibonacci-Generator Rekursiv")
+
+def fibR(n):
+  if n==1 or n==2:
+    return 1
+  return fibR(n-1)+fibR(n-2)
+
+fibR(0)
